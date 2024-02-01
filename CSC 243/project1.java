5a080579cb0bffbe8ebfecf8362644a8e637d6e6
Matthew Monn <matthewmonn1@yahoo.com>
2024-02-01 13:37:55 -0500

    Update

--- CSC 243/project1.java
+++ CSC 243/project1.java
@@ -3,6 +3,11 @@ import java.io.FileNotFoundException;
 import java.util.Scanner; 
 
 public class project1 {
+    public static void(String[] args) {
+        if (args.length > 0) {
+            
+        }
 
+    }
     
 }
