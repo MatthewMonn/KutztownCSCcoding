6f18e5d7717edc2834c4057251a3bd9c2a5d7bcb
Matthew Monn <matthewmonn1@yahoo.com>
2024-02-02 10:03:59 -0500

    U

--- CSC 243/project1.java
+++ CSC 243/project1.java
@@ -1,11 +1,10 @@
-import java.io.File; 
-import java.io.FileNotFoundException; 
-import java.util.Scanner; 
-
+// import java.io.File; 
+// import java.io.FileNotFoundException; 
+// java.util.Scanner; 
 public class project1 {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
         if (args.length > 0) {
-            system.out.println("Club Member: " + args[0]); 
+            System.out.println("Club Member: " + args[0]); 
         }
 
     }
